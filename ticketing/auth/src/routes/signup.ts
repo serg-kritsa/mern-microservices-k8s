0b3f5d2a1b6ef536decc0c794d23df182dd762ca
Serg <serg@example.com>
2021-07-14 01:04:32 +0300

    code style commit

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,18 +1,19 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
 router.post(
   "/api/users/signup",
   [
-    body("email").isEmail().withMessage("Email must be valid"),
+    body("email")
+      .isEmail()
+      .withMessage("Email must be valid"),
     body("password")
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage("Password must be between 4 and 20 characters"),
+      .withMessage("Password must be between 4 and 20 characters")
   ],
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
@@ -20,10 +21,6 @@ router.post(
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
-    console.log("Creating a user...");
-    throw new DatabaseConnectionError();
-
-    res.send({});
   }
 );
 
