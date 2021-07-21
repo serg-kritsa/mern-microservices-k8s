5d2dd39380d9d8b21c51e95f274314b224d09434
Serg <serg@example.com>
2021-07-21 23:31:36 +0300

    added TicketCreatedListener

---
+++ ticketing/orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,11 @@
+import { Message } from 'node-nats-streaming';
+import { Subjects, Listener, TicketCreatedEvent } from '@sknn/common';
+import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  queueGroupName = queueGroupName;
+
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
+}
