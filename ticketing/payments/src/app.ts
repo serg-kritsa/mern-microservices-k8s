48167d034ac48d96c7ce6c7335de04b271b9b634
Serg <serg@example.com>
2021-07-29 01:49:36 +0300

    added & tested POST /api/payments route

--- ticketing/payments/src/app.ts
+++ ticketing/payments/src/app.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError, currentUser } from '@cygnetops/common';
+import { errorHandler, NotFoundError, currentUser } from '@sknn/common';
+import { createChargeRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +15,8 @@ app.use(
   })
 );
 app.use(currentUser);
+
+app.use(createChargeRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
