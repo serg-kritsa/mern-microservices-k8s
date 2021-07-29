28a340d699fccf85234ba0f9fee6d9603a719b52
Serg <serg@example.com>
2021-07-30 01:26:52 +0300

    saved in db and tested

--- ticketing/payments/src/routes/__test__/new.test.ts
+++ ticketing/payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import { OrderStatus } from '@sknn/common';
 import { app } from '../../app';
 import { Order } from '../../models/order';
 import { stripe } from '../../stripe';
+import { Payment } from '../../models/payment';
 
 // jest.mock('../../stripe');
 
@@ -112,11 +113,17 @@ it('returns a 201 with valid inputs from real API', async () => {
     .expect(201);
 
   const stripeCharges = await stripe.charges.list({ limit: 50 });
-  const stripeCharge = stripeCharges.data.find((charge: { amount: number; }) => {
+  const stripeCharge = stripeCharges.data.find((charge) => {
     return charge.amount === price * 100;
   });
 
   expect(stripeCharge).toBeDefined();
   expect(stripeCharge!.currency).toEqual('usd');
+
+  const payment = await Payment.findOne({
+    orderId: order.id,
+    stripeId: stripeCharge!.id,
+  });
+  expect(payment).not.toBeNull();
 });
 
