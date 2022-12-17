ebdb6e03ccacf3964f1650fa539829bb9ed32e39
serg <serg@k.com>
2022-12-17 23:35:07 +0300

    unused parts just in case

--- ticketing/client/next.config.js
+++ ticketing/client/next.config.js
@@ -4,3 +4,11 @@ module.exports = {
     return config;
   },
 };
+// // NOTE: for the latest versions of Next.js
+// module.exports = {
+//   webpack: (config) => {
+//     config.watchOptions.poll = 300;
+//     return config;
+//   },
+// };
+// // NOTE: to update w/ new chages is needed to delete pod (kubectl delete pod <pod-name>) and restarted pod will be w/ new changes
